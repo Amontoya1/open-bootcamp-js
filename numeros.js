784eabf0604d6f5d8dfa0a630f863f88b9cf4c8b
andrea.montoya@ed.company <terceiro-aruiz@icatuseguros.com.br>
2022-06-19 15:52:13 -0300

    feat: adicionando ejercicios con numeros en javascript

---
+++ numeros.js
@@ -0,0 +1,13 @@
+let alturaCm = 179.5;
+let alturaM = 1.795;
+let pesoKg = 85.5;
+let alturaRoundUp = Math.ceil(alturaCm);
+let pesoRoundDown = Math.floor(pesoKg);
+let maxValor = Number.MAX_VALUE;
+
+console.log(alturaCm + " cm");
+console.log(alturaM + " m");
+console.log(pesoKg + " Kg");
+console.log(alturaRoundUp + " Cm");
+console.log(pesoRoundDown + " Kg");
+console.log(maxValor);
